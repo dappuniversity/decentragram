c3438469cffc534468c2aeed5d430e48e50978d4
sangya2001 <sangyasherpa2058@gmail.com>
2020-10-22 00:44:57 +0545

    Changed all the class component to functional component and added some comment on code for easier understanding

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,5 @@
 require('babel-register');
 require('babel-polyfill');
-require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
-const privateKeys = process.env.PRIVATE_KEYS || ""
 
 module.exports = {
   networks: {
@@ -11,17 +8,6 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(
-          privateKeys.split(','), // Array of account private keys
-          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
-        )
-      },
-      gas: 5000000,
-      gasPrice: 25000000000,
-      network_id: 3
-    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
